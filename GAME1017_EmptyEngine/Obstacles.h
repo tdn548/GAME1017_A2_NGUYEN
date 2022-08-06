e7d08ecd126f69cc1d4f16b397d455022cc43664
MHulfeldW <michael.hulfeld-wood@georgebrown.ca>
2022-08-06 16:08:26 -0400

    Some changes to obstacle not ready yet

--- GAME1017_EmptyEngine/Obstacles.h
+++ GAME1017_EmptyEngine/Obstacles.h
@@ -12,24 +12,24 @@ public:
 	Obstacle(SDL_Rect s, SDL_FRect d);
 	void Update();
 	void Render();
-	const SDL_FPoint& GetCenter() { return m_center; }
-	const double& GetRadius() { return m_radius; }
+	/*const SDL_FPoint& GetCenter() { return m_center; }
+	const double& GetRadius() { return m_radius; }*/
 
-	Uint8 GetRed() { return m_rMod; };
+	/*Uint8 GetRed() { return m_rMod; };
 	Uint8 GetGreen() { return m_gMod; };
-	Uint8 GetBlue() { return m_bMod; };
+	Uint8 GetBlue() { return m_bMod; };*/
 	double& GetDx() { return m_dx; }
 	double& GetDy() { return m_dy; }
 
-	void SetColMods(Uint8 r, Uint8 g, Uint8 b);
+	/*void SetColMods(Uint8 r, Uint8 g, Uint8 b);*/
 private:
-	static const int kScollSpeed = 1;
-	SDL_FPoint m_center;
+	static const int kScollSpeed = 5;
+	//SDL_FPoint m_center;
 	double m_angle,
-		m_dx, m_dy,
-		m_radius,
-		m_rotSpeed;
-	Uint8 m_rMod, m_gMod, m_bMod;
+		m_dx, m_dy;
+		/*m_radius,
+		m_rotSpeed;*/
+	/*Uint8 m_rMod, m_gMod, m_bMod;*/
 };
 
 class ObstacleField : public GameObject
